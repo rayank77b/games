a1739aab41a9ef9f8c23588a727f52cf5b6e1d54
Andrej Frank <frank.andrej.a@googlemail.com>
2023-09-07 19:25:12 +0200

    moved moving in class

--- breakout/breakout.py
+++ breakout/breakout.py
@@ -10,6 +10,7 @@ FPS = 30
 BRICK_W = 100   # width
 BRICK_H = 25    # height
 BALL_R = 10     # radius
+X_POS = 200     # start position of player
 
 # Define colors
 WHITE =  (255, 255, 255)
@@ -27,13 +28,34 @@ class Background(pygame.sprite.Sprite):
         self.rect = self.surf.get_rect()
 
 class Ball(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, vx, vy):
         super(Ball, self).__init__()
-        self.surf = pygame.Surface((BALL_R*2, BALL_R*2))
+        self.r = BALL_R
+        self.x = WIDTH/2
+        self.y = HEIGHT/2
+        self.vx = vx
+        self.vy = vy
+        self.pos = (self.x, self.y)
+        self.dt = clock.tick(FPS)
+        self.surf = pygame.Surface((self.r*2, self.r*2))
         self.surf.fill(BLACK)
-        pygame.draw.circle(self.surf, RED, (BALL_R, BALL_R), BALL_R)
+        pygame.draw.circle(self.surf, RED, (self.r, self.r), self.r)
         self.rect = self.surf.get_rect()
-
+    
+    def hit(self):
+        if (self.x >= WIDTH-self.r*2) or (self.x <= 0):
+            self.vx *= -1
+        if (self.y >= HEIGHT-self.r*2) or (self.y <= 0):
+            self.vy *= -1
+    
+    def move(self):
+        self.x += self.vx*self.dt
+        self.y += self.vy*self.dt
+        self.pos = (self.x, self.y)
+    
+    def drawme(self, screen):
+        screen.blit(self.surf, self.pos)
+        
 class Brick(pygame.sprite.Sprite):
     def __init__(self, hits=1):
         super(Brick, self).__init__()
@@ -49,14 +71,45 @@ class Brick(pygame.sprite.Sprite):
             return YELLOW
         else:
             return GREEN
+            
+    def drawme(self, screen):
+        screen.blit(self.surf, self.pos)
+
 
 class PlayerBrick(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, x, y):
         super(PlayerBrick, self).__init__()
-        self.surf = pygame.Surface((BRICK_W, BRICK_H))
+        self.w = BRICK_W
+        self.h = BRICK_H
+        self.x = x
+        self.y = y
+        self.pos = (x,y)
+        self.keypressed = False
+        self.vx = 0
+        self.surf = pygame.Surface((self.w, self.h))
         self.surf.fill(BLUE)
         self.rect = self.surf.get_rect()
+    
+    def keyevent(self, et, ek):
+        if et == KEYDOWN:
+            self.keypressed = True
+            if ek == K_RIGHT:
+                self.vx = 10 
+            elif ek == K_LEFT:
+                self.vx = -10
+            else:
+                self.vx = 0       # falls andere taste
+        else:
+            self.keypressed = False
+            self.key = 0
 
+    def move(self):  
+        if self.keypressed:
+            self.x += self.vx
+            self.pos = (self.x, self.y)
+    
+    def drawme(self, screen):
+        screen.blit(self.surf, self.pos)
             
 # Initialize Pygame
 pygame.init()
@@ -65,44 +118,36 @@ pygame.display.set_caption("Breakout")
 clock = pygame.time.Clock()
 
 background = Background()
-ball   = Ball()
-player = PlayerBrick()
+ball   = Ball(0.1, 0.3)
+player = PlayerBrick(X_POS, HEIGHT-BRICK_H-10)
 brick1 = Brick(3)
 brick2 = Brick(2)
 brick3 = Brick(1)
 
 gameOn = True
-X_POS = 200
-B_X   = 400
-B_Y   = 300
-V_X   = 1.2
-V_Y   = -0.3
 
 while gameOn:
     # get key event
     for event in pygame.event.get():
+        if event.type == KEYDOWN or event.type == KEYUP:
+            player.keyevent(event.type, event.key)
         if event.type == KEYDOWN:
-            if event.key == K_RIGHT:
-                X_POS = X_POS + 10
-            elif event.key == K_LEFT:
-                X_POS = X_POS - 10
-            elif event.key == K_q:
+            if event.key == K_q:
                 gameOn = False
         elif event.type == QUIT:
             gameOn = False
-    # calculate ball collision with wall
-    if (B_X >= WIDTH) or (B_X <= 0):
-        V_X *= -1
-    if (B_Y >= HEIGHT) or (B_Y <= 0):
-        V_Y *= -1
-    # calculate position of ball
-    dt = clock.tick(30)
-    B_X = B_X + V_X*dt
-    B_Y = B_Y + V_Y*dt
+    
+    player.move()
+    # get ball collision with wall
+    ball.hit()
+    # move ball
+    ball.dt = clock.tick(FPS)
+    ball.move()
+    
     # draw anything
     screen.blit(background.surf, (0, 0))
-    screen.blit(ball.surf, (B_X, B_Y))
-    screen.blit(player.surf, (X_POS, HEIGHT-BRICK_H-10))
+    ball.drawme(screen)
+    player.drawme(screen)
     screen.blit(brick1.surf, (10, 10))
     screen.blit(brick2.surf, (200, 50))
     screen.blit(brick3.surf, (350, 100))
